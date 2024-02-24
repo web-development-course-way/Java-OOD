6c0202adac2c32b05ce44c2afc83ec49224dde99
Mohamed <mohamedsabry109@gmail.com>
2024-02-24 15:15:36 +0200

    Add beans XML example

---
+++ Team Zero/Spring/beans-xml-example/src/main/java/com/example/demo/UserDao.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class UserDao {
+    public void save(User user) {
+        System.out.println("Saving user: " + user);
+        // Logic to save user to database
+    }
+}
